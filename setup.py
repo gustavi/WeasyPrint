80ee3007ea48eeebdbfcdbfeb1a4adc94f1045c5
Laville Augustin <gustavi@users.noreply.github.com>
2016-08-01 12:34:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README')).read()
 REQUIREMENTS = [
     # XXX: Keep this in sync with docs/install.rst
     'lxml>=3.0',
-    'html5lib>=0.999',
+    'html5lib==0.999',
     'tinycss==0.3',
     'cssselect>=0.6',
     'CairoSVG>=0.4.1',
